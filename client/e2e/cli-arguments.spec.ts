2402830d6670c987996aa2d2baf252e985a8d2fc
Richard Michael <rmichael@edgeofthenet.org>
2025-07-24 13:58:49 -0700

    Integration tests for command line arguments
    Use different server startup commands in Playwright to allow tests that
    command line arguments initialize form inputs as intended.
    
    A full integration test is used because the Inspector comprises multiple
    components (package bin entry point, start command, webClient, server),
    making benefit to test-effort higher, and less dependent on
    implementation details.
    
    The webServer start command uses the package entry point to ensure tests
    follow the exact user experience provided by: `npx @m/inspector [...]`
    
    Playwright's `webServer.command` cannot be [re]configured after it has
    started, so multiple Playwright invocations are necessary.  To do this,
    use an environment variable and a command-generating function (instead
    of multiple config files).
    
    Test tagging (`@cli`) is used to select which tests execute, while
    keeping the entire test suite together.
    
    Future:
    
    Playwright has a feature under consideration to permit a web server per
    project, which might simplify the config:
    https://github.com/microsoft/playwright/issues/22496

---
+++ client/e2e/cli-arguments.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+
+// Adjust the URL if your dev server runs on a different port
+const APP_URL = "http://localhost:6274/";
+
+test.describe("CLI npx (npm exec) start up", { tag: "@cli" }, () => {
+  test("CLI arguments should populate form fields", async ({ page }) => {
+    const cliTestScenario = process.env.CLI_TEST_SCENARIO;
+
+    // Skip this test if not running a CLI test scenario
+    if (
+      !cliTestScenario ||
+      (cliTestScenario !== "inline" && cliTestScenario !== "file")
+    ) {
+      test.skip();
+      return;
+    }
+
+    await page.goto(APP_URL);
+
+    const commandInput = page.getByRole("textbox", { name: "Command" });
+    const argsInput = page.getByRole("textbox", { name: "Arguments" });
+
+    await expect(commandInput).toHaveValue("npm");
+    await expect(argsInput).toHaveValue(
+      "--silent --prefix /path/to/mcp/servers/src/everything run start",
+    );
+  });
+});
